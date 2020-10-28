dd9ab767f0178f662a7c735415391c20b7cb1ebc
JesseStuy20 <jxie20@stuy.edu>
2020-10-28 16:10:20 -0400

    Added withdraw method and edited BankAccount code

--- BankAccount.java
+++ BankAccount.java
@@ -3,21 +3,29 @@ public class BankAccount {
   private int accountID ;
   private String password ;
 
-  public BankAccount(int accountID, String password) {
-    double balance = 0.0 ;
+  public BankAccount(int ID, String pass) {
+    accountID = ID ;
+    password = pass ;
+    balance = 0.00 ;
   }
 
   public double getBalance() {
     return balance ;
   }
 
-  public double getAccountID() {
+  public int getAccountID() {
     return accountID ;
   }
 
   public boolean deposit(double amount) {
     if (amount > 0) balance = balance + amount;
     if (amount > 0) return true;
+    else return false;
+  }
+
+  public boolean withdraw(double amount) {
+    if ((amount > 0) && (balance >= amount)) balance = balance - amount;
+    if ((amount > 0) && (balance >= amount)) return true;
     else return false;
   }
 
