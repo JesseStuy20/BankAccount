31961c8a55c202f4dbf02b468cf2f95ffbc537bf
JesseStuy20 <jxie20@stuy.edu>
2020-10-28 14:35:31 -0400

    Added private variables, constructor, and getBalance method

---
+++ BankAccount.java
@@ -0,0 +1,14 @@
+public class BankAccount {
+  private double balance ;
+  private int accountID ;
+  private String password ;
+
+  public BankAccount(int accountID, String password) {
+    double balance = 0.0 ;
+  }
+
+  public double getBalance() {
+    return balance ;
+  }
+
+}
